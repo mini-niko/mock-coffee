821f6a4e569100924ac6dce3b42bf8026076344d
Maurício Xavier Oliveira <xavierdeoliveiramauricio@gmail.com>
2024-10-07 13:17:27 -0300

    fix: fix CORS perms

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+        ]
+      }
+    ]
+  },
   reactStrictMode: true,
 }
 
